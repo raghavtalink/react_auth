cfb001356d8b6bacd9b25379250a25da39559049
raghavtalink <raghav.kundra@talink.io>
2024-09-20 16:43:11 +0530

    Bug fixing.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
-//updated line of code for testing purposes try infinite for testing the new feature.
 const userSchema = new Schema({
     username:{
         type: String,
