b7173071f16482e3f6f71069c8dfbfbb0e463d73
raghavtalink <raghav.kundra@talink.io>
2024-09-20 20:08:25 +0530

    added comments in Login Screen

--- src/components/login.js
+++ src/components/login.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
+/**
+ * Form component for user login
+ * 
+ * @prop {function} onLoginSuccess - function to be called when login is successful
+ * 
+ * @returns {React.ReactElement} - JSX for the login form
+ */
 function LoginForm({ onLoginSuccess }) {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
+    /**
+     * Handles form submission
+     * 
+     * @param {object} data - form data
+     * 
+     * @returns {Promise<void>} - resolves when request is finished
+     */
     const onSubmit = async (data) => {
         try {
             const response = await axios.post('https://react-auth-t80o.onrender.com/api/login', data); //this is my API Implementation
@@ -15,30 +29,36 @@ function LoginForm({ onLoginSuccess }) {
         }
     };
 
+    // render the login form
     return (
         <div className="max-w-lg w-full bg-white p-10 border border-gray-300 rounded-lg shadow-lg">
             <h2 className="text-3xl font-bold mb-6 text-center">Login</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-6">
+                    {/* username field */}
                     <label className="block text-gray-700 text-lg">Username</label>
                     <input 
                         type="text" 
                         {...register("username", { required: "Username is required" })}
                         className="w-full p-3 border border-gray-300 rounded mt-2"
                     />
+                    {/* show error message if username is invalid */}
                     {errors.username && <p className="text-red-500 text-sm mt-1">{errors.username.message}</p>}
                 </div>
 
                 <div className="mb-6">
+                    {/* password field */}
                     <label className="block text-gray-700 text-lg">Password</label>
                     <input 
                         type="password" 
                         {...register("password", { required: "Password is required" })}
                         className="w-full p-3 border border-gray-300 rounded mt-2"
                     />
+                    {/* show error message if password is invalid */}
                     {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
                 </div>
 
+                {/* submit button */}
                 <button 
                     type="submit" 
                     className="w-full bg-black text-white p-3 rounded-lg hover:bg-slate-900 transition duration-200"
@@ -51,3 +71,4 @@ function LoginForm({ onLoginSuccess }) {
 }
 
 export default LoginForm;
+
