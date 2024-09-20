8621e2b75f7523f5f49e0fcde8eb63f6cc153012
raghavtalink <raghav.kundra@talink.io>
2024-09-20 19:53:13 +0530

    added API implementation comments

--- src/components/login.js
+++ src/components/login.js
@@ -7,7 +7,7 @@ function LoginForm({ onLoginSuccess }) {
 
     const onSubmit = async (data) => {
         try {
-            const response = await axios.post('https://react-auth-t80o.onrender.com/api/login', data);
+            const response = await axios.post('https://react-auth-t80o.onrender.com/api/login', data); //this is my API Implementation
             localStorage.setItem('token', response.data.token);
             onLoginSuccess();
         } catch (error) {
